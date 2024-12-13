9b96b90fd47ad93b4fa97e2129d84bcc59a9e0f0
Serpil-Dndr <serpil.dndrca@gmail.com>
2024-12-12 22:28:00 -0500

    update nav bar color

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #c6d42ad7;
+  background-color: #10d177d7;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
