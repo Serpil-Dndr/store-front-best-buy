ca61ef0b3ca906025ef23423d6a502cabe493cd5
Serpil-Dndr <serpil.dndrca@gmail.com>
2024-12-12 22:19:25 -0500

    changed nav bar color to the yellow

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #0689d5d7;
+  background-color: #c6d42ad7;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
